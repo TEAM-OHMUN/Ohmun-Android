a5a4c73f40e789116521e797a4bfd5ece5608f43
wonseok <dnjstjr245@gmail.com>
2022-12-21 10:29:12 +0900

    [feat] util object 클래스들 정의

---
+++ app/src/main/java/com/bongyang/ohmun/util/Constants.kt
@@ -0,0 +1,24 @@
+package com.bongyang.ohmun.util
+
+object Constants {
+
+    const val BASE_URL = "http://10.0.2.2:8080"
+
+    const val DETAILS_ARGUMENT_KEY = "movieId"
+
+    const val OHMUN_DATABASE = "ohmun_database"
+    const val MOVIE_DATABASE_TABLE = "movie_table"
+    const val ACTOR_DATABASE_TABLE = "actor_table"
+    const val REVIEW_DATABASE_TABLE = "review_table"
+
+
+    const val MOVIE_REMOTE_KEY_DATABASE_TABLE = "movie_remote_keys_table"
+
+    const val PREFERENCES_NAME = "ohmun_preferences"
+    const val PREFERENCES_KEY = "on_boarding_completed"
+
+    const val LAST_ON_BOARDING_PAGE = 3
+
+    const val ITEMS_PER_PAGE = 3
+    const val MIN_BACKGROUND_IMAGE_HEIGHT = 0.4f
+}
