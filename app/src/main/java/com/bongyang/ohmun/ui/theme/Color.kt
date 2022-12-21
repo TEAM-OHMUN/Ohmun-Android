741281a2b39e6e1fb8e76835ef9a96cf74dfb069
wonseok <dnjstjr245@gmail.com>
2022-12-21 10:28:44 +0900

    [feat] compose ui theme 파일들 정의

---
+++ app/src/main/java/com/bongyang/ohmun/ui/theme/Color.kt
@@ -0,0 +1,62 @@
+package com.bongyang.ohmun.ui.theme
+
+import androidx.compose.material.Colors
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Color.Companion.Black
+import androidx.compose.ui.graphics.Color.Companion.Blue
+import androidx.compose.ui.graphics.Color.Companion.White
+
+val Purple200 = Color(0xFFBB86FC)
+val Purple500 = Color(0xFF6200EE)
+val Purple700 = Color(0xFF3700B3)
+val Teal200 = Color(0xFF03DAC5)
+
+val LightGray = Color(0xFFD8D8D8)
+val DarkGray = Color(0xFF2A2A2A)
+val StarColor = Color(0xFFFFC94D)
+val ModernBlue = Color(0xFF366ED5)
+
+val ShimmerLightGray = Color(0xFFF1F1F1)
+val ShimmerMediumGray = Color(0xFFE3E3E3)
+val ShimmerDarkGray = Color(0xFF1D1D1D)
+
+val Colors.statusBarColor
+    get() = if (isLight) Color.White else Color.Black
+
+val Colors.welcomeScreenBackgroundColor
+    @Composable
+    get() = if (isLight) White else Color.Black
+
+val Colors.titleColor
+    @Composable
+    get() = if (isLight) DarkGray else LightGray
+
+val Colors.descriptionColor
+    @Composable
+    get() = if (isLight) DarkGray.copy(alpha = 0.5f)
+    else LightGray.copy(alpha = 0.5f)
+
+val Colors.activeIndicatorColor
+    @Composable
+    get() = if (isLight) Purple500 else Purple700
+
+val Colors.inactiveIndicatorColor
+    @Composable
+    get() = if (isLight) LightGray else DarkGray
+
+val Colors.buttonBackgroundColor
+    @Composable
+    get() = if (isLight) Purple500 else Purple700
+
+val Colors.topAppBarContentColor
+    @Composable
+    get() = if (isLight) DarkGray else LightGray
+
+val Colors.topAppBarBackgroundColor
+    @Composable
+    get() = if (isLight) White else Color.Black
+
+val Colors.genreContentColor
+    @Composable
+    get() = if (isLight) Black else White
---
+++ app/src/main/java/com/bongyang/ohmun/ui/theme/Dimens.kt
@@ -0,0 +1,22 @@
+package com.bongyang.ohmun.ui.theme
+
+import androidx.compose.ui.unit.dp
+
+val EXTRA_LARGE_PADDING = 40.dp
+val LARGE_PADDING = 20.dp
+val MEDIUM_PADDING = 16.dp
+val SMALL_PADDING = 10.dp
+val EXTRA_SMALL_PADDING = 6.dp
+
+val TOP_APP_BAR_HEIGHT = 56.dp
+val PAGING_INDICATOR_WIDTH = 12.dp
+val PAGING_INDICATOR_SPACING = 8.dp
+val TITLE_PLACEHOLDER_HEIGHT = 30.dp
+val GENRE_PLACEHOLDER_HEIGHT = 15.dp
+val ACTORS_PLACEHOLDER_HEIGHT = 50.dp
+
+val INFO_ICON_SIZE = 40.dp
+
+val MOVIE_ITEM_HEIGHT = 400.dp
+val MIN_SHEET_HEIGHT = 140.dp
+val EXPANDED_RADIUS_LEVEL = 0.dp
