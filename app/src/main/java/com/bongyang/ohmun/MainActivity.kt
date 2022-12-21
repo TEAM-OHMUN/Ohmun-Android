362eb0c3801da81881f18635a4997dbd1da36486
wonseok <dnjstjr245@gmail.com>
2022-12-21 10:29:49 +0900

    [feat] MainActivity, GlobalApp 구현

---
+++ app/src/main/java/com/bongyang/ohmun/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.bongyang.ohmun
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.material.ExperimentalMaterialApi
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+import coil.annotation.ExperimentalCoilApi
+import com.bongyang.ohmun.navigation.SetupNavGraph
+import com.bongyang.ohmun.ui.theme.OhmunTheme
+import com.google.accompanist.pager.ExperimentalPagerApi
+import dagger.hilt.android.AndroidEntryPoint
+
+@ExperimentalMaterialApi
+@ExperimentalCoilApi
+@ExperimentalAnimationApi
+@ExperimentalPagerApi
+@AndroidEntryPoint
+class MainActivity : ComponentActivity() {
+
+    private lateinit var navController: NavHostController
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            OhmunTheme {
+                navController = rememberNavController()
+                SetupNavGraph(navController = navController)
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/bongyang/ohmun/OhmunApplication.kt
@@ -0,0 +1,7 @@
+package com.bongyang.ohmun
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class OhmunApplication: Application()
